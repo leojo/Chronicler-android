0f07eba645706a3e4bf30646a079a8bd9af4b2de
BjornBjarnsteins <bjb46@hi.is>
2016-02-10 17:01:02 +0000

    added main_menu to campaign activity

--- app/src/main/java/com/android/chronicler/ui/CampaignActivity.java
+++ app/src/main/java/com/android/chronicler/ui/CampaignActivity.java
@@ -2,6 +2,8 @@ package com.android.chronicler.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -59,5 +61,28 @@ public class CampaignActivity extends AppCompatActivity {
         });
         // --------------------------------------
         Intent intent = getIntent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // TODO: Maybe use a different menu here
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
