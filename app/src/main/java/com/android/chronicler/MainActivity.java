5ea4a0e68215c512088734fb326990596d50d49e
andreabjork <andreabjork43@gmail.com>
2016-03-04 13:03:27 +0000

    Fixed login redirect AGAIN, please be careful if mergeing

--- app/src/main/java/com/android/chronicler/MainActivity.java
+++ app/src/main/java/com/android/chronicler/MainActivity.java
@@ -19,6 +19,7 @@ import com.android.chronicler.util.DataLoader;
 import com.android.chronicler.util.UserLocalStore;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
@@ -29,15 +30,19 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.cookie.Cookie;
+import cz.msebera.android.httpclient.impl.cookie.BasicClientCookie;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private UserLocalStore store;
     private DataLoader loader;
+    private static PersistentCookieStore cookieStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +50,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         store = new UserLocalStore(getApplicationContext());
         loader = new DataLoader();
-        if(!store.userInSession()) {
+        cookieStore = new PersistentCookieStore(this);
+        boolean inSession = userInSession(cookieStore.getCookies());
+        if(!inSession) {
             redirectToLogin();
-            Log.i("LOGIN", "Seem to be logged in, this is the cookie: " + store.getUserCookie());
         }
     }
+
+    public boolean userInSession(List<Cookie> cookies) {
+        Cookie userCookie = new BasicClientCookie("user", null);
+        for(Cookie c : cookies) {
+            if(c.getName().equals("user")) userCookie = c;
+        }
+        return (userCookie.getValue() != null);
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
