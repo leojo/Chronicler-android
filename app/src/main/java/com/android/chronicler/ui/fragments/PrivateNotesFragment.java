a48c42a8a7a155c5c763a7d1f126a1899b80f416
BjornBjarnsteins <bjb46@hi.is>
2016-04-13 19:56:46 +0000

    added private notes front end functionality

--- app/src/main/java/com/android/chronicler/ui/fragments/PrivateNotesFragment.java
+++ app/src/main/java/com/android/chronicler/ui/fragments/PrivateNotesFragment.java
@@ -1,17 +1,22 @@
 package com.android.chronicler.ui.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.android.chronicler.R;
+import com.android.chronicler.ui.CampaignNoteActivity;
 
 import java.util.ArrayList;
 
@@ -25,7 +30,8 @@ public class PrivateNotesFragment extends SheetFragment {
     private static final int maxItemLength = 35;
     private String campaignName;
     private ArrayList<String> privateNotes;
-    private ListAdapter adapter;
+    private ArrayList<String> shortNotes;
+    private ArrayAdapter<String> adapter;
 
     public PrivateNotesFragment() {
         // Required empty public constructor
@@ -56,12 +62,13 @@ public class PrivateNotesFragment extends SheetFragment {
         // Inflate the layout for this fragment
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_campaign_players, container, false);
 
-        ArrayList<String> shortNotes = new ArrayList<>();
+        // TODO: Possibly filter out new lines here as well
+        shortNotes = new ArrayList<>();
         for (String privateNote : privateNotes) {
-            shortNotes.add(privateNote.substring(0, maxItemLength).concat("..."));
+            shortNotes.add(trimToLength(privateNote));
         }
 
-        ListView playerListView = (ListView) rootView.findViewById(R.id.player_list);
+        ListView noteListView = (ListView) rootView.findViewById(R.id.player_list);
         adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_expandable_list_item_1,
                 shortNotes);
@@ -72,10 +79,50 @@ public class PrivateNotesFragment extends SheetFragment {
         addButtonView.setPadding(20, 20, 20, 20);
         addButtonView.setImageResource(R.drawable.ic_add_circle_24dp);
 
-        playerListView.addFooterView(addButtonView);
-        playerListView.setAdapter(adapter);
+        noteListView.addFooterView(addButtonView);
+        noteListView.setAdapter(adapter);
+
+        noteListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Adapter", String.valueOf(adapter.getCount()));
+                if (position == adapter.getCount()) {
+                    openNote("", position);
+                } else {
+                    openNote(privateNotes.get(position), position);
+                }
+            }
+        });
 
         return rootView;
+    }
+
+    private void openNote(String noteText, int position) {
+        Intent intent = new Intent(getActivity(), CampaignNoteActivity.class);
+        intent.putExtra("TEXT", noteText);
+        startActivityForResult(intent, position);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("Notes", "Received text: " + data.getStringExtra("TEXT"));
+        String newNote = data.getStringExtra("TEXT");
+        if (requestCode < privateNotes.size()) {
+            privateNotes.set(requestCode, newNote);
+            shortNotes.set(requestCode, trimToLength(newNote));
+        } else {
+            privateNotes.add(requestCode, newNote);
+            shortNotes.add(requestCode, trimToLength(newNote));
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private String trimToLength(String str) {
+        if (str.length() >= maxItemLength) {
+            return str.substring(0, maxItemLength).replace("\n", " ").concat("...");
+        } else {
+            return str;
+        }
     }
 
 }
